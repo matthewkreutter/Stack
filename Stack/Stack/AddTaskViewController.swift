fe65c54f870156a933cf524596a85066ca729b97
Marc VandenBerg <mvandenberg@wustl.edu>
2017-11-19 22:33:04 -0600

    message

--- Stack/Stack/AddTaskViewController.swift
+++ Stack/Stack/AddTaskViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class AddTaskViewController: UIViewController {
+class AddTaskViewController: UIViewController, UIPickerViewDelegate {
     
     @IBOutlet weak var taskName: UITextField!
     @IBOutlet weak var category: UITextField!
@@ -18,11 +18,82 @@ class AddTaskViewController: UIViewController {
     @IBOutlet weak var time: UITextField!
     @IBOutlet weak var reminder: UITextField!
     var tasks: [String] = []
+    var categoryOption = ["Homework", "Chores", "Errands", "Miscellaneous"]
+    var importanceOption = ["1","2","3","4","5","6","7","8","9","10"]
+    var reminderOption = ["1 Day","2 Days","3 Days","4 Days","5 Days", "6 Days", "7 Days"]
+    
+    let datePicker = UIDatePicker()
+    let timePicker = UIDatePicker()
+    let categoryView = UIPickerView()
+    let importanceView = UIPickerView()
+    let reminderView = UIPickerView()
     
     override func viewDidLoad() {
         if let myTasks = UserDefaults.standard.array(forKey: "tasks") {
             tasks = myTasks as! [String]
         }
+        createDatePicker()
+        createTimePicker()
+
+        categoryView.delegate = self
+        category.inputView = categoryView
+        importanceView.delegate = self
+        importance.inputView = importanceView
+        reminderView.delegate = self
+        reminder.inputView = reminderView
+        
+    }
+    
+    func createDatePicker() {
+        datePicker.datePickerMode = .date
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolBar.setItems([doneButton], animated: false)
+        date.inputAccessoryView = toolBar
+        date.inputView = datePicker
+    }
+    
+    func createTimePicker() {
+        timePicker.datePickerMode = .time
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let doneTimeButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneTimePressed))
+        toolBar.setItems([doneTimeButton], animated: false)
+        time.inputAccessoryView = toolBar
+        time.inputView = timePicker
+    }
+    
+    @objc func donePressed() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .none
+        date.text = dateFormatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    @objc func doneTimePressed() {
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateStyle = .none
+        timeFormatter.timeStyle = .short
+        time.text = timeFormatter.string(from: timePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    func numberOfComponentsInPickerView(_ pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categoryOption.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categoryOption[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        category.text = categoryOption[row]
+        category.endEditing(true)
     }
     
     @IBAction func addButtonTapped(_ sender: Any) {
--- Stack/Stack/EditTaskViewController.swift
+++ Stack/Stack/EditTaskViewController.swift
@@ -9,13 +9,89 @@
 import Foundation
 import UIKit
 
-class EditTaskViewController: UIViewController {
+class EditTaskViewController: UIViewController, UIPickerViewDelegate {
     var taskName: String?
     var category: String?
     var importance: Int?
     var date: String?
     var time: String?
     var reminder: String?
- 
+    var categoryOption = ["Homework", "Chores", "Errands", "Miscellaneous"]
+    var importanceOption = ["1","2","3","4","5","6","7","8","9","10"]
+    var reminderOption = ["1 Day","2 Days","3 Days","4 Days","5 Days", "6 Days", "7 Days"]
     
+    @IBOutlet weak var categoryField: UITextField!
+    @IBOutlet weak var importanceField: UITextField!
+    @IBOutlet weak var dateField: UITextField!
+    @IBOutlet weak var timeField: UITextField!
+    @IBOutlet weak var reminderField: UITextField!
+    let datePicker = UIDatePicker()
+    let timePicker = UIDatePicker()
+    let categoryView = UIPickerView()
+    let importanceView = UIPickerView()
+    let reminderView = UIPickerView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createDatePicker()
+        createTimePicker()
+        categoryView.delegate = self
+        categoryField.inputView = categoryView
+        importanceView.delegate = self
+        importanceField.inputView = importanceView
+        reminderView.delegate = self
+        reminderField.inputView = reminderView
+    }
+    
+    func createDatePicker() {
+        datePicker.datePickerMode = .date
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolBar.setItems([doneButton], animated: false)
+        dateField.inputAccessoryView = toolBar
+        dateField.inputView = datePicker
+    }
+    
+    func createTimePicker() {
+        timePicker.datePickerMode = .time
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let doneTimeButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneTimePressed))
+        toolBar.setItems([doneTimeButton], animated: false)
+        timeField.inputAccessoryView = toolBar
+        timeField.inputView = timePicker
+    }
+    
+    @objc func donePressed() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .none
+        dateField.text = dateFormatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    @objc func doneTimePressed() {
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateStyle = .none
+        timeFormatter.timeStyle = .short
+        timeField.text = timeFormatter.string(from: timePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    func numberOfComponentsInPickerView(_ pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categoryOption.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categoryOption[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        categoryField.text = categoryOption[row]
+        categoryField.endEditing(true)
+    }
 }
